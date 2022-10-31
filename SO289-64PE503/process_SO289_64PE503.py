377cc72bbf53ccb755dde5ffd02cdae9d1feb17a
Louise Delaigue <louisedelaigue@gmail.com>
2022-10-31 13:12:08 +0100

    black formatting

--- SO289-64PE503/process_SO289_64PE503.py
+++ SO289-64PE503/process_SO289_64PE503.py
@@ -17,24 +17,24 @@ logfile = ks.read_logfile(
 dbs = ks.read_dbs("data/64PE503_SO289_2022.dbs", logfile=logfile)
 
 # Fix datetime issue in .dbs samples
-dbs.loc[dbs["bottle"]=="SO289-41297", "analysis_datetime"] = "2022-10-21 11:58:00"
-dbs.loc[dbs["bottle"]=="64PE503-28-5-8", "analysis_datetime"] = "2022-10-21 12:12:00"
-dbs.loc[dbs["bottle"]=="64PE503-68-6-5", "analysis_datetime"] = "2022-10-21 12:44:00"
-dbs.loc[dbs["bottle"]=="SO289-40698", "analysis_datetime"] = "2022-10-21 13:00:00"
-dbs.loc[dbs["bottle"]=="64PE503-46-4-8", "analysis_datetime"] = "2022-10-21 13:30:00"
-dbs.loc[dbs["bottle"]=="64PE503-56-4-2", "analysis_datetime"] = "2022-10-21 13:46:00"
-dbs.loc[dbs["bottle"]=="SO289-40611", "analysis_datetime"] = "2022-10-21 14:18:00"
-dbs.loc[dbs["bottle"]=="SO289-40499", "analysis_datetime"] = "2022-10-21 14:33:00"
-dbs.loc[dbs["bottle"]=="64PE503-58-4-5", "analysis_datetime"] = "2022-10-21 14:50:00"
-dbs.loc[dbs["bottle"]=="64PE503-66-5-8", "analysis_datetime"] = "2022-10-21 15:09:00"
-dbs.loc[dbs["bottle"]=="64PE503-14-4-11", "analysis_datetime"] = "2022-10-21 15:24:00"
-dbs.loc[dbs["bottle"]=="64PE503-47-4-11", "analysis_datetime"] = "2022-10-21 15:39:00"
-dbs.loc[dbs["bottle"]=="64PE503-26-5-8", "analysis_datetime"] = "2022-10-21 15:55:00"
-dbs.loc[dbs["bottle"]=="CRM-189-0526-01", "analysis_datetime"] = "2022-10-21 16:28:00"
-dbs.loc[dbs["bottle"]=="CRM-189-0526-02", "analysis_datetime"] = "2022-10-21 17:17:00"
+dbs.loc[dbs["bottle"] == "SO289-41297", "analysis_datetime"] = "2022-10-21 11:58:00"
+dbs.loc[dbs["bottle"] == "64PE503-28-5-8", "analysis_datetime"] = "2022-10-21 12:12:00"
+dbs.loc[dbs["bottle"] == "64PE503-68-6-5", "analysis_datetime"] = "2022-10-21 12:44:00"
+dbs.loc[dbs["bottle"] == "SO289-40698", "analysis_datetime"] = "2022-10-21 13:00:00"
+dbs.loc[dbs["bottle"] == "64PE503-46-4-8", "analysis_datetime"] = "2022-10-21 13:30:00"
+dbs.loc[dbs["bottle"] == "64PE503-56-4-2", "analysis_datetime"] = "2022-10-21 13:46:00"
+dbs.loc[dbs["bottle"] == "SO289-40611", "analysis_datetime"] = "2022-10-21 14:18:00"
+dbs.loc[dbs["bottle"] == "SO289-40499", "analysis_datetime"] = "2022-10-21 14:33:00"
+dbs.loc[dbs["bottle"] == "64PE503-58-4-5", "analysis_datetime"] = "2022-10-21 14:50:00"
+dbs.loc[dbs["bottle"] == "64PE503-66-5-8", "analysis_datetime"] = "2022-10-21 15:09:00"
+dbs.loc[dbs["bottle"] == "64PE503-14-4-11", "analysis_datetime"] = "2022-10-21 15:24:00"
+dbs.loc[dbs["bottle"] == "64PE503-47-4-11", "analysis_datetime"] = "2022-10-21 15:39:00"
+dbs.loc[dbs["bottle"] == "64PE503-26-5-8", "analysis_datetime"] = "2022-10-21 15:55:00"
+dbs.loc[dbs["bottle"] == "CRM-189-0526-01", "analysis_datetime"] = "2022-10-21 16:28:00"
+dbs.loc[dbs["bottle"] == "CRM-189-0526-02", "analysis_datetime"] = "2022-10-21 17:17:00"
 
 # Fix datetime issue in .dbs for sample "64PE503-12-7-2" // because there's a duplicate
-L = (dbs["dic_cell_id"] == "C_Oct21-22_0810") & (dbs["bottle"]=="64PE503-12-7-2")
+L = (dbs["dic_cell_id"] == "C_Oct21-22_0810") & (dbs["bottle"] == "64PE503-12-7-2")
 dbs.loc[L, "analysis_datetime"] = "2022-10-21 13:14:00"
 
 # Fix datetime issue in .dbs nuts
@@ -50,9 +50,9 @@ dbs["analysis_datenum"] = dates.date2num(dbs["analysis_datetime"])
 
 # Add a column to locate real analysis days
 dbs["real_day"] = True
-dbs.loc[dbs["dic_cell_id"]=="C_Aug26-22_0808", "real_day"] = False
-dbs.loc[dbs["dic_cell_id"]=="C_Aug28-22_0808", "real_day"] = False
-dbs.loc[dbs["dic_cell_id"]=="C_Aug30-22_0808", "real_day"] = False
+dbs.loc[dbs["dic_cell_id"] == "C_Aug26-22_0808", "real_day"] = False
+dbs.loc[dbs["dic_cell_id"] == "C_Aug28-22_0808", "real_day"] = False
+dbs.loc[dbs["dic_cell_id"] == "C_Aug30-22_0808", "real_day"] = False
 
 # Create empty metadata columns
 for meta in [
@@ -78,7 +78,7 @@ for b in crm_batches:
         dbs.loc[L, "total_phosphate"] = 0.45  # micromol/kg-sw
         dbs.loc[L, "total_silicate"] = 2.1  # micromol/kg-sw
         dbs.loc[L, "total_ammonium"] = 0  # micromol/kg-sw
-    
+
     if b == 195:
         L = dbs["bottle"].str.startswith("CRM-{}".format(b))
         dbs.loc[L, "dic_certified"] = 2024.96  # micromol/kg-sw
@@ -87,52 +87,60 @@ for b in crm_batches:
         dbs.loc[L, "total_phosphate"] = 0.49  # micromol/kg-sw
         dbs.loc[L, "total_silicate"] = 3.6  # micromol/kg-sw
         dbs.loc[L, "total_ammonium"] = 0  # micromol/kg-sw
-    
+
 # Assign temperature = 25.0 for VINDTA analysis temperature
 dbs["temperature_override"] = 25.0
 
 # Assign metadata for junks
-dbs['salinity'] = dbs['salinity'].fillna(35)
-dbs['total_phosphate'] = dbs['total_phosphate'].fillna(0)
-dbs['total_silicate'] = dbs['total_silicate'].fillna(0)
-dbs['total_ammonium'] = dbs['total_ammonium'].fillna(0)
+dbs["salinity"] = dbs["salinity"].fillna(35)
+dbs["total_phosphate"] = dbs["total_phosphate"].fillna(0)
+dbs["total_silicate"] = dbs["total_silicate"].fillna(0)
+dbs["total_ammonium"] = dbs["total_ammonium"].fillna(0)
 
 # Add optional column "file_good"
 dbs["file_good"] = True
 
-L = ((dbs["bottle"]=="NUTSLAB05") 
-     & (dbs["analysis_datetime"].dt.month==10)
-     & (dbs["analysis_datetime"].dt.day==28))
+L = (
+    (dbs["bottle"] == "NUTSLAB05")
+    & (dbs["analysis_datetime"].dt.month == 10)
+    & (dbs["analysis_datetime"].dt.day == 28)
+)
 dbs.loc[L, "file_good"] = False
 
-L = ((dbs["bottle"]=="NUTSLAB03") 
-     & (dbs["analysis_datetime"].dt.month==10)
-     & (dbs["analysis_datetime"].dt.day==25))
+L = (
+    (dbs["bottle"] == "NUTSLAB03")
+    & (dbs["analysis_datetime"].dt.month == 10)
+    & (dbs["analysis_datetime"].dt.day == 25)
+)
 dbs.loc[L, "file_good"] = False
 
 # Add a flag column
 # where good = 4, questionable = 3, bad = 2
 dbs["flag"] = 4
 
-L = ((dbs["bottle"]=="NUTSLAB05") 
-     & (dbs["analysis_datetime"].dt.month==10)
-     & (dbs["analysis_datetime"].dt.day==28))
+L = (
+    (dbs["bottle"] == "NUTSLAB05")
+    & (dbs["analysis_datetime"].dt.month == 10)
+    & (dbs["analysis_datetime"].dt.day == 28)
+)
 dbs.loc[L, "flag"] = 2
 
-L = ((dbs["bottle"]=="NUTSLAB03") 
-     & (dbs["analysis_datetime"].dt.month==10)
-     & (dbs["analysis_datetime"].dt.day==25))
+L = (
+    (dbs["bottle"] == "NUTSLAB03")
+    & (dbs["analysis_datetime"].dt.month == 10)
+    & (dbs["analysis_datetime"].dt.day == 25)
+)
 dbs.loc[L, "file_good"] = 2
 
-dbs.loc[dbs["bottle"]=="64PE503-10-5-2", "flag"] = 3 # weird DIC
-dbs.loc[dbs["bottle"]=="SO289-41003", "flag"] = 3 # weird DIC
-dbs.loc[dbs["bottle"]=="64PE503-53-9-3", "flag"] = 3 # red tape
-dbs.loc[dbs["bottle"]=="64PE503-53-4-5", "flag"] = 3 # red tape
-dbs.loc[dbs["bottle"]=="64PE503-53-4-6", "flag"] = 3 # red tape
-dbs.loc[dbs["bottle"]=="64PE503-53-4-9", "flag"] = 3 # red tape
-dbs.loc[dbs["bottle"]=="64PE503-57-3-2", "flag"] = 3 # red tape
-dbs.loc[dbs["bottle"]=="64PE503-53-4-2", "flag"] = 3 # red tape
-dbs.loc[dbs["bottle"]=="SO289-40060", "flag"] = 3 # bottle popped, DIC only 1 x rinse
+dbs.loc[dbs["bottle"] == "64PE503-10-5-2", "flag"] = 3  # weird DIC
+dbs.loc[dbs["bottle"] == "SO289-41003", "flag"] = 3  # weird DIC
+dbs.loc[dbs["bottle"] == "64PE503-53-9-3", "flag"] = 3  # red tape
+dbs.loc[dbs["bottle"] == "64PE503-53-4-5", "flag"] = 3  # red tape
+dbs.loc[dbs["bottle"] == "64PE503-53-4-6", "flag"] = 3  # red tape
+dbs.loc[dbs["bottle"] == "64PE503-53-4-9", "flag"] = 3  # red tape
+dbs.loc[dbs["bottle"] == "64PE503-57-3-2", "flag"] = 3  # red tape
+dbs.loc[dbs["bottle"] == "64PE503-53-4-2", "flag"] = 3  # red tape
+dbs.loc[dbs["bottle"] == "SO289-40060", "flag"] = 3  # bottle popped, DIC only 1 x rinse
 
 # Flag any nan
 if dbs["counts"].isnull().any():
@@ -145,9 +153,18 @@ dbs["file_path"] = "data/64PE503_SO289_2022/"
 
 # Assign TA acid batches
 dbs["analysis_batch"] = 0
-dbs.loc[(dbs['analysis_datetime'].dt.day >= 11) & (dbs['analysis_datetime'].dt.month == 10), 'analysis_batch'] = 1
-dbs.loc[(dbs['analysis_datetime'].dt.day >= 20) & (dbs['analysis_datetime'].dt.month == 10), 'analysis_batch'] = 2
-dbs.loc[(dbs['analysis_datetime'].dt.day >= 28) & (dbs['analysis_datetime'].dt.month == 10), 'analysis_batch'] = 3
+dbs.loc[
+    (dbs["analysis_datetime"].dt.day >= 11) & (dbs["analysis_datetime"].dt.month == 10),
+    "analysis_batch",
+] = 1
+dbs.loc[
+    (dbs["analysis_datetime"].dt.day >= 20) & (dbs["analysis_datetime"].dt.month == 10),
+    "analysis_batch",
+] = 2
+dbs.loc[
+    (dbs["analysis_datetime"].dt.day >= 28) & (dbs["analysis_datetime"].dt.month == 10),
+    "analysis_batch",
+] = 3
 
 # Select which TA CRMs to use/avoid for calibration
 dbs["reference_good"] = ~np.isnan(dbs.alkalinity_certified)
@@ -156,24 +173,39 @@ dbs["reference_good"] = ~np.isnan(dbs.alkalinity_certified)
 calk.io.get_VINDTA_filenames(dbs)
 calk.dataset.calibrate(dbs)
 calk.dataset.solve(dbs)
-calk.plot.titrant_molinity(dbs, figure_fname="figs/titrant_molinity.png", show_bad=False)
-calk.plot.alkalinity_offset(dbs, figure_fname="figs/alkalinity_offset.png", show_bad=False)
+calk.plot.titrant_molinity(
+    dbs, figure_fname="figs/titrant_molinity.png", show_bad=False
+)
+calk.plot.alkalinity_offset(
+    dbs, figure_fname="figs/alkalinity_offset.png", show_bad=False
+)
 
 # === DIC
 # Add optional column "blank_good
-dbs['blank_good'] = True
-dbs.loc[dbs["bottle"]=="CRM-189-0526-02", "blank_good"] = False
-dbs.loc[dbs["bottle"]=="64PE503-10-5-2", "blank_good"] = False
-dbs.loc[dbs["bottle"]=="SO289-41003", "blank_good"] = False
-dbs.loc[dbs["bottle"]=="SO289-41003", "blank_good"] = False
+dbs["blank_good"] = True
+dbs.loc[dbs["bottle"] == "CRM-189-0526-02", "blank_good"] = False
+dbs.loc[dbs["bottle"] == "64PE503-10-5-2", "blank_good"] = False
+dbs.loc[dbs["bottle"] == "SO289-41003", "blank_good"] = False
+dbs.loc[dbs["bottle"] == "SO289-41003", "blank_good"] = False
 
-dbs.loc[(dbs["bottle"]=="JUNK01") & (dbs["dic_cell_id"]=="C_Oct24-22_0810"), "blank_good"] = False
-dbs.loc[(dbs["bottle"]=="JUNK04") & (dbs["dic_cell_id"]=="C_Oct28-22_0910"), "blank_good"] = False
-dbs.loc[(dbs["bottle"]=="NUTSLAB05") & (dbs["dic_cell_id"]=="C_Oct28-22_0910"), "blank_good"] = False
+dbs.loc[
+    (dbs["bottle"] == "JUNK01") & (dbs["dic_cell_id"] == "C_Oct24-22_0810"),
+    "blank_good",
+] = False
+dbs.loc[
+    (dbs["bottle"] == "JUNK04") & (dbs["dic_cell_id"] == "C_Oct28-22_0910"),
+    "blank_good",
+] = False
+dbs.loc[
+    (dbs["bottle"] == "NUTSLAB05") & (dbs["dic_cell_id"] == "C_Oct28-22_0910"),
+    "blank_good",
+] = False
 
 # Select which DIC CRMs to use/avoid for calibration --- only fresh bottles
 dbs["k_dic_good"] = dbs.crm & dbs.bottle.str.startswith("CRM")
-dbs.loc[dbs.crm & dbs.bottle.str.endswith("-02"), "k_dic_good"] = False # remove CRMs used twice for DIC calibration
+dbs.loc[
+    dbs.crm & dbs.bottle.str.endswith("-02"), "k_dic_good"
+] = False  # remove CRMs used twice for DIC calibration
 
 # Get blanks and apply correction
 dbs.get_blank_corrections()
@@ -187,8 +219,8 @@ dbs.plot_dic_offset(figure_path="figs/")
 
 # === ENTIRE DATASET
 # Correct bottle name typos
-dbs.loc[dbs["bottle"]=="CRM-189-0897", "bottle"] = "CRM-189-0897-01"
-dbs.loc[dbs["bottle"]=="64PE503-65-2-1", "bottle"] = "64PE503-65-1-2"
+dbs.loc[dbs["bottle"] == "CRM-189-0897", "bottle"] = "CRM-189-0897-01"
+dbs.loc[dbs["bottle"] == "64PE503-65-2-1", "bottle"] = "64PE503-65-1-2"
 
 # Demote dbs to a standard DataFrame
 dbs = pd.DataFrame(dbs)
@@ -213,8 +245,8 @@ colors = itertools.cycle(
     )
 )
 
-#%% Only keep nuts bottles that have file_good
-L = (dbs["bottle"].str.startswith("NUTS")) & (dbs["file_good"]==True)
+# Only keep nuts bottles that have file_good
+L = (dbs["bottle"].str.startswith("NUTS")) & (dbs["file_good"] == True)
 nuts = dbs[L]
 
 # Only keep real analysis days
@@ -225,7 +257,7 @@ real_days = list(nuts["dic_cell_id"].unique())
 
 # Create an hour column
 # nuts["hour"] = nuts["analysis_datetime"].dt.hour
-nuts["datetime"] = nuts['analysis_datetime'].dt.strftime('%H:%M')
+nuts["datetime"] = nuts["analysis_datetime"].dt.strftime("%H:%M")
 nuts["datetime"] = pd.to_datetime(nuts["datetime"])
 
 # Create figure
@@ -247,11 +279,11 @@ for r in real_days:
         alpha=0.3,
         label=l,
     )
-    
-myFmt = mdates.DateFormatter('%H')
-ax.xaxis.set_major_formatter(myFmt)    
 
-ax.legend(loc="upper left", ncol=2) # bbox_to_anchor=(1, 0.5)
+myFmt = mdates.DateFormatter("%H")
+ax.xaxis.set_major_formatter(myFmt)
+
+ax.legend(loc="upper left", ncol=2)  # bbox_to_anchor=(1, 0.5)
 # ax.set_ylim(2000, 2200)
 ax.grid(alpha=0.3)
 ax.set_xlabel("Time (hrs)")
@@ -260,4 +292,3 @@ ax.set_ylabel("$DIC$ / μmol · $kg^{-1}$")
 # Save plot
 plt.tight_layout()
 plt.savefig("./figs/day_nuts.png")
-
