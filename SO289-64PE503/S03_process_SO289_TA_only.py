ecf899579ed95682e5c12187fa3bf83e2e1faadd
Louise Delaigue <louisedelaigue@gmail.com>
2022-12-08 11:49:19 +0100

    script run

--- SO289-64PE503/S03_process_SO289_TA_only.py
+++ SO289-64PE503/S03_process_SO289_TA_only.py
@@ -1,8 +1,5 @@
-import numpy as np, pandas as pd, koolstof as ks, calkulate as calk
-import copy, itertools
-from pandas.tseries.offsets import DateOffset
+import pandas as pd, numpy as np, koolstof as ks, calkulate as calk
 import matplotlib.dates as mdates
-from matplotlib import pyplot as plt
 
 # Import logfile and dbs file
 logfile = ks.read_logfile(
@@ -20,7 +17,6 @@ dbs["analysis_datenum"] = mdates.date2num(dbs["analysis_datetime"])
 # Create empty metadata columns
 for meta in [
     "salinity",
-    "dic_certified",
     "alkalinity_certified",
     "total_phosphate",
     "total_silicate",
@@ -35,7 +31,6 @@ crm_batches = [189, 195, 198]
 for b in crm_batches:
     if b == 189:
         L = dbs["bottle"].str.startswith("CRM-{}".format(b))
-        dbs.loc[L, "dic_certified"] = 2009.48  # micromol/kg-sw
         dbs.loc[L, "alkalinity_certified"] = 2205.26  # micromol/kg-sw
         dbs.loc[L, "salinity"] = 33.494
         dbs.loc[L, "total_phosphate"] = 0.45  # micromol/kg-sw
@@ -44,7 +39,6 @@ for b in crm_batches:
 
     if b == 195:
         L = dbs["bottle"].str.startswith("CRM-{}".format(b))
-        dbs.loc[L, "dic_certified"] = 2024.96  # micromol/kg-sw
         dbs.loc[L, "alkalinity_certified"] = 2213.51  # micromol/kg-sw
         dbs.loc[L, "salinity"] = 33.485
         dbs.loc[L, "total_phosphate"] = 0.49  # micromol/kg-sw
@@ -53,7 +47,6 @@ for b in crm_batches:
 
     if b == 198:
         L = dbs["bottle"].str.startswith("CRM-{}".format(b))
-        dbs.loc[L, "dic_certified"] = 2033.64  # micromol/kg-sw
         dbs.loc[L, "alkalinity_certified"] = 2200.67  # micromol/kg-sw
         dbs.loc[L, "salinity"] = 33.504
         dbs.loc[L, "total_phosphate"] = 0.67  # micromol/kg-sw
@@ -71,7 +64,7 @@ dbs["total_ammonium"] = dbs["total_ammonium"].fillna(0)
 
 # Assign alkalinity metadata
 dbs["analyte_volume"] = 98.865  # TA pipette volume in ml
-dbs["file_path"] = "data/64PE503_SO289_2022/"
+dbs["file_path"] = "data/TA_ONLY/SO289_TA_only/"
 
 # Assign TA acid batches
 dbs["analysis_batch"] = 0
@@ -92,10 +85,10 @@ calk.io.get_VINDTA_filenames(dbs)
 calk.dataset.calibrate(dbs)
 calk.dataset.solve(dbs)
 calk.plot.titrant_molinity(
-    dbs, figure_fname="figs/titrant_molinity.png", show_bad=False
+    dbs, figure_fname="figs/SO289-TA_only/titrant_molinity.png", show_bad=False
 )
 calk.plot.alkalinity_offset(
-    dbs, figure_fname="figs/alkalinity_offset.png", show_bad=False
+    dbs, figure_fname="figs/SO289-TA_only/alkalinity_offset.png", show_bad=False
 )
 
 # Demote dbs to a standard DataFrame
